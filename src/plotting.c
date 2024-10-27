dbaba7428756d39cf60e24c62a164eef121cc4c3
Night <146769399+Jakkarin-Promsee@users.noreply.github.com>
2024-10-27 20:26:29 +0700

    Refactor plotting.c: wrap the main function in for(1) loop for continuous execution and clean some code condition

--- src/plotting.c
+++ src/plotting.c
@@ -34,120 +34,134 @@ void clear_screen()
 int main()
 {
     setup();
-    FILE *fpt;
-    char data_path[50];
+    clear_screen();
 
-    char on_plotting = 1;
-    int data_mode = 0;
-    int plot_type = 0;
-    int plot_count = 0;
+    int plot_type = 0, data_mode = 0; // Use to manage plotting function
+    char data_path[50];               // Use to keep csv path
+    FILE *fpt;                        // Use to keep csv content from path
+    int plot_count = 0;               // Use to indicate currect ploting point of data
 
-    printf("Type of your plot:\n");
-    printf("Table (0), Scatter (1), Line (2) (default 0) :");
-    scanf("%d", &plot_type);
-    printf("write (0) read (1)", "\033[4m", "\033[0m");
-    scanf("%d", &data_mode);
-    if (data_mode == 1)
+    while (1)
     {
-        printf("Choose your data path (ex: your_path/file.csv):");
-        scanf("%s", data_path);
-    }
-    while ((getchar()) != '\n')
-        ;
+        // Initialize Plotting, Get plot_type and data_mode
+        printf("Type of your plot:\n");
+        printf("Table (0), Scatter (1), Line (2), Exit(-1) : ");
+        scanf("%d", &plot_type);
+        if (plot_type == -1)
+            break;
+        printf("write (0) read (1), Exit(-1) : ");
+        scanf("%d", &data_mode);
+        if (data_mode == -1)
+            break;
 
-    char buffer[100], lower_buffer[100];
-    double x_values[200];
-    double y_values[200];
-
-    if (plot_type == 1)
-    {
-        // Force Insert Point (0,0)
-        x_values[plot_count] = 0;
-        y_values[plot_count] = 0;
-        plot_count++;
-
+        // Get csv path
         if (data_mode == 1)
         {
-            fpt = fopen(data_path, "r");
-            if (fpt == NULL)
+            printf("Choose your data path (ex: your_path/file.csv): ");
+            scanf("%s", data_path);
+        }
+
+        // Start Plotting function
+        char buffer[100], lower_buffer[100];
+        double x_values[200];
+        double y_values[200];
+
+        if (plot_type == 1)
+        {
+            // Force Insert Point (0,0)
+            x_values[plot_count] = 0;
+            y_values[plot_count] = 0;
+
+            if (data_mode == 1)
             {
-                printf("Error opening file.\n");
-                return 1;
-            }
-            char line[100];
-            while (fgets(line, sizeof(line), fpt))
-            {
-                // printf("%s\n", line);
-                if (sscanf(line, "%lf,%lf", &x_values[plot_count], &y_values[plot_count]) == 2)
+                // Open csv path
+                fpt = fopen(data_path, "r");
+                if (fpt == NULL)
                 {
+                    printf("Error opening file '%s'.\n\n", data_path);
+                    continue;
+                }
+
+                // Get x,y from csv with maximum 100 character of data per line
+                char line[100];
+                while (fgets(line, sizeof(line), fpt))
+                {
+                    if (sscanf(line, "%lf,%lf", &x_values[plot_count], &y_values[plot_count]) == 2)
+                        plot_count++;
+                }
+                fclose(fpt);
+            }
+        }
+
+        // Clear input buffer
+        while (getchar() != '\n')
+            ;
+
+        printf("Getting Input (Press ENTER if done)\n");
+        while (1)
+        {
+            // Display the plot
+            if (plot_count > 1)
+            {
+                switch (plot_type)
+                {
+                case 0:
+                    plot_table(x_values, y_values, plot_count, SCREEN_H, SCREEN_W);
+                    break;
+                case 1:
+                    plot_scatter(x_values, y_values, plot_count, SCREEN_H, SCREEN_W);
+                    break;
+                case 2:
+                    // plot_scatter(axis, i);
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            // Show plotting history table
+            printf("\n\n");
+            printf("------+--------+---------\n");
+            printf("index | X      | Y\n");
+            printf("------+--------+---------\n");
+            for (int j = 0; j < plot_count; j++)
+            {
+                printf("%3d   | %-6.2lf | %.2lf\n", j, x_values[j], y_values[j]);
+            }
+            printf("------+--------+---------\n");
+
+            // Get new plotting point
+            printf("\nPlot %d [x] [y], Exit(exit): ", plot_count);
+            if (fgets(buffer, sizeof(buffer), stdin))
+            {
+                strcpy(lower_buffer, buffer);
+                strlwr(lower_buffer);
+
+                // Check exit condition
+                if (!strcmp(lower_buffer, "done\n") ||
+                    !strcmp(lower_buffer, "exit\n") ||
+                    !strcmp(lower_buffer, "esc\n") ||
+                    !strcmp(lower_buffer, "\n"))
+                {
+                    printf("Do you really want to exit the program? (%sy%s, n) ", "\033[4m", "\033[0m");
+                    fgets(buffer, sizeof(buffer), stdin);
+                    if (buffer[0] == '\n' || tolower(buffer[0]) == 'y')
+                        clear_screen();
+                    break;
+                }
+
+                else if (sscanf(buffer, "%lf %lf", &x_values[plot_count], &y_values[plot_count]) == 2)
+                {
+                    // printf("%.2lf %.2lf\n", x_values[i], y_values[i]);
+                    clear_screen();
                     plot_count++;
                 }
-            }
-            fclose(fpt);
-        }
-    }
-
-    printf("Getting Input (Press ENTER if done)\n");
-    while (on_plotting)
-    {
-        // Display the plot
-        if (plot_count > 1)
-        {
-            switch (plot_type)
-            {
-            case 0:
-                plot_table(x_values, y_values, plot_count, SCREEN_H, SCREEN_W);
-                break;
-            case 1:
-                plot_scatter(x_values, y_values, plot_count, SCREEN_H, SCREEN_W);
-                break;
-            case 2:
-                // plot_scatter(axis, i);
-                break;
-            default:
-                break;
-            }
-        }
-        // plotting history
-        printf("\n\n");
-        printf("------+--------+---------\n");
-        printf("index | X      | Y\n");
-        printf("------+--------+---------\n");
-        for (int j = 0; j < plot_count; j++)
-        {
-            printf("%3d   | %-6.2lf | %.2lf\n", j, x_values[j], y_values[j]);
-        }
-        printf("------+--------+---------\n");
-
-        printf("\nPlot %d [x] [y] ", plot_count);
-        // Recieve Plotting
-        if (fgets(buffer, sizeof(buffer), stdin))
-        {
-            strcpy(lower_buffer, buffer);
-            strlwr(lower_buffer);
-
-            if (strcmp(lower_buffer, "done\n") == 0 || strcmp(lower_buffer, "exit\n") == 0 || strcmp(lower_buffer, "esc\n") == 0 || strcmp(lower_buffer, "\n") == 0)
-            {
-                char buffer_[10], exit = 'y';
-                printf("Do you really want to exit the program? (%sY%s, n) ", "\033[4m", "\033[0m");
-                fgets(buffer_, sizeof(buffer_), stdin);
-                if (buffer_[0] == '\n' || tolower(buffer_[0]) == 'y')
+                else
                 {
-                    on_plotting = 0;
-                    return 0;
+                    printf("Invalid input. Please enter two valid numbers.\n");
                 }
             }
-
-            else if (sscanf(buffer, "%lf %lf", &x_values[plot_count], &y_values[plot_count]) == 2)
-            {
-                // printf("%.2lf %.2lf\n", x_values[i], y_values[i]);
-                system("cls");
-                plot_count++;
-            }
-            else
-            {
-                printf("Invalid input. Please enter two valid numbers.\n");
-            }
         }
     }
+    printf("End of Program, Thank you.\n");
 }
